3d305f8e3a2867258d3c103805ff8d4114c1d6b6
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-03-09 17:40:44 -0800

    add facebook pixel for tracking and ad purposes

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@
 				$('#menuToggle').toggleClass('active');
 				$('body').toggleClass('body-push-toleft');
 				$('#theMenu').toggleClass('menu-open');
-			});
+        if( $(e.target).is('a') ) {
+        $(this).toggleClass('menu-close');
+        };
+      });
 
 
 })(jQuery)
